329ff39737d8178863bd00a416b2b24e3e40438b
nicolas-trouin <nicolas.trouin@insa-lyon.fr>
2020-02-10 16:23:20 +0100

    Ex 1 - Rebours avec sleep() et getpid()

---
+++ rebours.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+int main(int argc, char** argv)
+{
+    printf("Bonjour je suis '%s'\n",argv[0]);
+	unsigned pid = getpid();
+	printf("%d: debut\r\n", pid);
+	unsigned i = atoi(argv[1]);
+	assert(i>0);
+	for(; i > 0; --i){
+		sleep(1);
+		printf("%d: %d\r\n", pid, i);
+	}
+    return 0;
+}
